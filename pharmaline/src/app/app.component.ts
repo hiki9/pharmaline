73b39f3eaf6fa52374122b4e8dd3984de83f8dc2
hiki9 <ccsprivepa@hotmail.com>
2021-06-12 15:07:47 +0200

    2
    2

--- pharmaline/src/app/app.component.ts
+++ pharmaline/src/app/app.component.ts
@@ -18,82 +18,76 @@ export class AppComponent {
                     [
                         {
                             label: 'Consultation liste des médicament',
-                            items: [{label: 'La liste complète',routerLink :'medicaments'}, {label: 'Video 1.2'}]
+                            items: [{label: 'La liste complète',routerLink :'medicaments'}]
                         },
                         {
-                            label: 'Video 2',
-                            items: [{label: 'Video 2.1'}, {label: 'Video 2.2'}]
+                            label: 'les catégories de médicament',
+                            items: [{label: 'catégorie'}, ]
                         }
+                    ],
+                   
+                ]
+            },
+            {
+                label: 'pharmacie', icon: 'pi pi-fw pi-users',
+                items: [
+                    [
+                        {
+                            label: 'adresse',
+                            items: [{label: 'adresse 1.1', routerLink :'pharmacie'}]
+                        },
+                        
                     ],
                     [
                         {
-                            label: 'Video 3',
-                            items: [{label: 'Video 3.1'}, {label: 'Video 3.2'}]
+                            label: 'code',
+                            items: [{label: 'code de la pharmacie'}]
                         },
-                        {
-                            label: 'Video 4',
-                            items: [{label: 'Video 4.1'}, {label: 'Video 4.2'}]
-                        }
+                       
+                    ],
+                    [
+                       
                     ]
                 ]
             },
             {
-                label: 'Users', icon: 'pi pi-fw pi-users',
+                label: 'pharmaciens', icon: 'pi pi-fw pi-calendar',
                 items: [
                     [
                         {
-                            label: 'User 1',
-                            items: [{label: 'User 1.1'}, {label: 'User 1.2'}]
+                            label: 'nom',
+                            items: [{label: 'nom du pharmacien',routerLink :'pharmaciens'}]
                         },
                         {
-                            label: 'User 2',
-                            items: [{label: 'User 2.1'}, {label: 'User 2.2'}]
-                        },
-                    ],
-                    [
-                        {
-                            label: 'User 3',
-                            items: [{label: 'User 3.1'}, {label: 'User 3.2'}]
+                            label: 'prénom',
+                            items: [{label: 'prénom du pharmacien'}]
                         },
                         {
-                            label: 'User 4',
-                            items: [{label: 'User 4.1'}, {label: 'User 4.2'}]
+                            label: 'code',
+                            items: [{label: 'code du pharmacien'}]
                         }
                     ],
                     [
-                        {
-                            label: 'User 5',
-                            items: [{label: 'User 5.1'}, {label: 'User 5.2'}]
-                        },
-                        {
-                            label: 'User 6',
-                            items: [{label: 'User 6.1'}, {label: 'User 6.2'}]
-                        }
+                       
                     ]
                 ]
             },
             {
-                label: 'Events', icon: 'pi pi-fw pi-calendar',
+                label: 'catégories', icon: 'pi pi-fw pi-calendar',
                 items: [
                     [
                         {
-                            label: 'Event 1',
-                            items: [{label: 'Event 1.1'}, {label: 'Event 1.2'}]
+                            label: 'nom',
+                            items: [{label: 'nom de la catégorie',routerLink :'categories'}]
                         },
+                       
                         {
-                            label: 'Event 2',
-                            items: [{label: 'Event 2.1'}, {label: 'Event 2.2'}]
+                            label: 'code',
+                            items: [{label: 'code de la catégorie'}]
                         }
                     ],
                     [
-                        {
-                            label: 'Event 3',
-                            items: [{label: 'Event 3.1'}, {label: 'Event 3.2'}]
-                        },
-                        {
-                            label: 'Event 4',
-                            items: [{label: 'Event 4.1'}, {label: 'Event 4.2'}]
-                        }
+                       
                     ]
                 ]
             },
@@ -115,10 +109,7 @@ export class AppComponent {
                         }
                     ],
                     [
-                        {
-                            label: 'Technology 4',
-                            items: [{label: 'Setting 4.1'}, {label: 'Setting 4.2'}]
-                        }
+                        
                     ]
                 ]
             }
--- pharmaline/src/app/app.module.ts
+++ pharmaline/src/app/app.module.ts
@@ -9,13 +9,15 @@ import {MegaMenuModule} from 'primeng/megamenu';
 import { PharmacieComponent } from './pharmacie/pharmacie.component';
 import { PharmaciensComponent } from './pharmaciens/pharmaciens.component';
 import { MedicamentsComponent } from './medicaments/medicaments.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PharmacieComponent,
     PharmaciensComponent,
-    MedicamentsComponent
+    MedicamentsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
